3e0d3288600283265d6fc18cc30fc5f687d6498a
claranceatgalvanize <claranceatgalvanize@gmail.com>
2019-05-12 21:47:30 -0700

    work in progress. It Works, but it's not dry.

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 (function() {
   'use strict';
+  document.addEventListener("DOMContentLoaded", function(event) {
+    
+    let green = document.querySelector ('#goButton');
+    let yellow = document.querySelector ('#slowButton');
+    let red = document.querySelector ('#stopButton');
 
-  // YOUR CODE HERE
+    let greenLight = document.querySelector ('#goLight');
+    let yellowLight = document.querySelector ('#slowLight');
+    let redLight = document.querySelector ('#stopLight');
+
+    const mouseEnteredGo = () => console.log(`Entered ${green.innerHTML} button`);
+    green.addEventListener('mouseenter', mouseEnteredGo)
+    const go = () => greenLight.classList.toggle("go");
+    green.addEventListener('click', go)
+
+    const mouseEnteredSlow = () => console.log(`Entered ${yellow.innerHTML} button`);
+    yellow.addEventListener('mouseenter', mouseEnteredSlow)
+    const slow = () => yellowLight.classList.toggle("slow");
+    yellow.addEventListener('click', slow);
+
+    const mouseEnteredStop = () => console.log(`Entered ${red.innerHTML} button`);
+    red.addEventListener('mouseenter', mouseEnteredStop)
+    const stop = () => redLight.classList.toggle("stop");
+    red.addEventListener('click', stop);
+
+
+
+  }); // DOM CONTENT LOADED
 })();
